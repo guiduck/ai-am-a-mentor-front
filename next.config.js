cf996f4a07b18621245a2f2bae6a4820159cd312
guilherme <guiduck02@gmail.com>
2025-11-14 16:55:15 -0300

    try to fix build

--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Force standalone output (NOT export) for server-side features
-  output: 'standalone',
   images: {
     domains: ['images.unsplash.com', 'via.placeholder.com'],
   },
